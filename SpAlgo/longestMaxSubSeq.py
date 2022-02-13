7d7cdbd164da09bdb4145e7f8e1c0d6f5c123ad8
Amir Shamsi <emirshamsi.uni@gmail.com>
2022-02-13 22:23:20 +0330

    change name

--- SpAlgo/longestMaxSubSeq.py
+++ SpAlgo/longestMaxSubSeq.py
@@ -1,6 +1,6 @@
 from typing import Union
 
-def find_max_subsequence_in_array(seq: list[Union[int, float]]) -> tuple[Union[float, int], list[int]]:
+def longestMaxSubSeq(seq: list[Union[int, float]]) -> tuple[Union[float, int], list[int]]:
     # variables
     _current_sum = 0
     max_sum = -float('inf')
