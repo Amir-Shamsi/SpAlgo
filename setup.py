ad8cac4e812f316112707b5ab671b46cd593c60d
Amir Shamsi <emirshamsi.uni@gmail.com>
2022-02-13 21:14:22 +0330

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,4 +3,8 @@ from setuptools import find_packages, setup
 setup(
     name='SpAlgo',
     packages=find_packages(include=['SpAlgo']),
+
+    version='0.1.0',
+    description='SpAlgo has some special algorithms to help you in your education and your tasks.',
+    author='Amir Shamsi',
 )
