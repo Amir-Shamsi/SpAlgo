b0d9f2fa2833a4197746371458afcb17d9aede94
Amir Shamsi <emirshamsi.uni@gmail.com>
2022-02-13 21:20:36 +0330

    Update _find_max_value_subsequence.py

--- SpAlgo/_find_max_value_subsequence.py
+++ SpAlgo/_find_max_value_subsequence.py
@@ -1,6 +1,5 @@
 from typing import Union
 
-
 def find_max_subsequence_in_array(seq: list[Union[int, float]]) -> tuple[Union[float, int], list[int]]:
     # variables
     _current_sum = 0
@@ -21,11 +20,11 @@ def find_max_subsequence_in_array(seq: list[Union[int, float]]) -> tuple[Union[f
        :return: returns a tuple of following information->
             |---- 1. first element is the max sum of the sequence
             +-----2. the second is a list of  start and end index
-       :var _current_sum: every element of list we go through will
+       :type _current_sum: every element of list we go through will
             be added to this variable
-       :var _indexes: will hold the first index of the the subsequence
-       :var max_sum: will hold the max sum of the longest subsequence
-       :var _last_index: will hold the last index of subsequence
+       :type _indexes: will hold the first index of the the subsequence
+       :type max_sum: will hold the max sum of the longest subsequence
+       :type _last_index: will hold the last index of subsequence
     """
     for _index in range(len(seq)):
         _current_sum += seq[_index]
