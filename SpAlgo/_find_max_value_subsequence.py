88ed0d1741cf4441d275f24d84b70395d6e70298
Amir Shamsi <emirshamsi.uni@gmail.com>
2022-02-13 20:20:07 +0330

    Update _find_max_value_subsequence.py

--- SpAlgo/_find_max_value_subsequence.py
+++ SpAlgo/_find_max_value_subsequence.py
@@ -2,3 +2,8 @@ from typing import Union
 
 
 def find_max_subsequence_in_array(seq: list[Union[int, float]]) -> tuple[Union[float, int], list[int]]:
+    # variables
+    _current_sum = 0
+    max_sum = -float('inf')
+    _indexes = []
+    _last_index = -1
