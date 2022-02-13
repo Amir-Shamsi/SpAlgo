684929e78d44fc4cff979238a299eb6f27aa099e
Amir Shamsi <emirshamsi.uni@gmail.com>
2022-02-13 21:14:03 +0330

    Update test_find_max_value_subsequence.py

--- tests/test_find_max_value_subsequence.py
+++ tests/test_find_max_value_subsequence.py
@@ -5,3 +5,4 @@ def test_find_max_value_subsequence():
                  1.38030105,  0.01133961, -0.72806449,  0.6211909,  -0.49041476, -2.13556865,
                  -0.07442468,  0.59530056, -1.98011873,  0.61534235,  0.88164606,  1.54469791,
                  -1.68645546,  0.11235733]
+    assert _find_max_value_subsequence.find_max_subsequence_in_array(_sequence) == (5.20187816, [1, 7])
