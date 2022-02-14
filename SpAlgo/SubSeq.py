ccbd34839d13b73d5888ae4b709d81054625f7c2
Amir Shamsi <emirshamsi.uni@gmail.com>
2022-02-14 10:42:10 +0330

    create the class function `__calculate_max_longest_subsequence`

--- SpAlgo/SubSeq.py
+++ SpAlgo/SubSeq.py
@@ -20,3 +20,24 @@ class LongestMax:
             :param sequence: sequence of numbers which will be given at first
         """
         self.sequence = sequence
+    def __calculate_max_longest_subsequence(self):
+        # variables
+        _current_sum = 0
+        max_sum = -float('inf')
+        _indexes = []
+        _last_index = -1
+        for _index in range(len(self.sequence)):
+            _current_sum += self.sequence[_index]
+            _indexes.append(_index)
+            if _current_sum < 0:
+                _current_sum = 0
+                _indexes.clear()
+            if _current_sum > max_sum:
+                max_sum = _current_sum
+                _last_index = _index
+
+        self._maximum_summation = max_sum
+        self.start_index = _indexes[0]
+        self.end_index = _last_index
+        return
+
