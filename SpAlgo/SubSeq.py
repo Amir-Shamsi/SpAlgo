9b72c5b2a54161090618846936693f9065b3c341
Amir Shamsi <emirshamsi.uni@gmail.com>
2022-02-14 10:40:26 +0330

    create class `__init__`function

--- SpAlgo/SubSeq.py
+++ SpAlgo/SubSeq.py
@@ -8,3 +8,15 @@ class LongestMax:
     """
         :var sequence: sequence of numbers which will be given at first
     """
+
+    def __init__(self, sequence: list[Union[int, float]]):
+        """
+            The given sequence is a list of type int or float by the
+             other word must be a numerical list.
+
+            All list's elements can have different types:
+                * float
+                * integer
+            :param sequence: sequence of numbers which will be given at first
+        """
+        self.sequence = sequence
