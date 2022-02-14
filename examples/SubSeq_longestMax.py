142c83b6cfbc41be6029a4e96d363355ada30999
Amir Shamsi <emirshamsi.uni@gmail.com>
2022-02-14 10:48:39 +0330

    add some needed things into files

--- examples/SubSeq_longestMax.py
+++ examples/SubSeq_longestMax.py
@@ -1,1 +1,13 @@
 from SpAlgo import SubSeq
+
+# find maximum value of longest subsequence in a list of numbers.
+
+# color code to make the output look stoning
+ORANGE_COLOR = '\033[94m'
+
+# code of make the text bold
+BOLD_START = '\033[1m'
+
+# code of making text to go back to the normal
+NORM = '\033[0m'
+
