56fef38ed42023778d8740183a6e4c9c0c866090
Amir Shamsi <emirshamsi.uni@gmail.com>
2022-02-14 10:49:06 +0330

    sequence example

--- examples/SubSeq_longestMax.py
+++ examples/SubSeq_longestMax.py
@@ -11,3 +11,7 @@ BOLD_START = '\033[1m'
 # code of making text to go back to the normal
 NORM = '\033[0m'
 
+# example of sequence of numerical data
+data_sequence = [-1, 3, -2.2, 5, 2, 12.1, -23, 4, 8, 6.5, 4, -10, 12, 15]
+
+
