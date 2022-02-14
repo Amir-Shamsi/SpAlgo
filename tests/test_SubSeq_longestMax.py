fb31a5964754fdd5c81de535ee38af8000789455
Amir Shamsi <emirshamsi.uni@gmail.com>
2022-02-14 13:54:55 +0330

    Update test_SubSeq_longestMax.py

--- tests/test_SubSeq_longestMax.py
+++ tests/test_SubSeq_longestMax.py
@@ -5,12 +5,12 @@ def test_findLongestMaxSubSeq():
                  1.38030105,  0.01133961, -0.72806449,  0.6211909,  -0.49041476, -2.13556865,
                  -0.07442468,  0.59530056, -1.98011873,  0.61534235,  0.88164606,  1.54469791,
                  -1.68645546,  0.11235733]
-    res = SubSeq.LongestMax(_sequence)
+    _res = SubSeq.LongestMax(_sequence)
 
     # checking the outputs
-    assert res.maximum_sum() == 5.20187816
-    assert res.start_index == 1
-    assert res.end_index == 7
+    assert _res.maximum_sum() == 5.20187816
+    assert _res.start_index == 1
+    assert _res.end_index == 7
 
-    assert res.subsequence() == [0.36545751,  0.34194459,  1.17144381, 0.81346123,  1.11793036,
+    assert _res.subsequence() == [0.36545751,  0.34194459,  1.17144381, 0.81346123,  1.11793036,
                                  1.38030105,  0.01133961]
