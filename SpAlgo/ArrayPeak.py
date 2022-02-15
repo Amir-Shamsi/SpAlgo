c139fc26f12d9f18b86bb71333c8bfa4c765ed6c
Mohammad Keshavarzi <keshavari.m380@gmail.com>
2022-02-15 11:53:17 +0330

    Changin lenght to size variable inside ArrayPeak.py

--- SpAlgo/ArrayPeak.py
+++ SpAlgo/ArrayPeak.py
@@ -3,28 +3,30 @@ from typing import Union
 
 class ArrayPeak:
     _inner_array = []
+    _size = 0
     """
     :var _inner_array: 
     """
     def __init__(self, array: list[Union[int, float]]):
         self._inner_array = array
+        self._size = len(self._inner_array)
 
     def findPeak(self) -> tuple[Union[int, float], int]:
         """
         :return: a tuple of peak value and index
         """
         # if there is one element in array return it
-        if len(self._inner_array) < 2:
+        if self._size < 2:
             return self._inner_array[0], 0
     
         # if the first or last element of data array
         if self._inner_array[0] > self._inner_array[1]:
             return self._inner_array[0], 0
-        if self._inner_array[(len(self._inner_array) - 1)] > self._inner_array[(len(self._inner_array) - 2)]:
-            return self._inner_array[(len(self._inner_array) - 1)], (len(self._inner_array) - 1)
+        if self._inner_array[(self._size - 1)] > self._inner_array[(self._size - 2)]:
+            return self._inner_array[(self._size - 1)], (self._size - 1)
     
         # otherwise
-        pointer = len(self._inner_array) // 2
+        pointer = self._size // 2
         while True:
             current_element = self._inner_array[pointer]
             if self._inner_array[pointer + 1] > current_element:
