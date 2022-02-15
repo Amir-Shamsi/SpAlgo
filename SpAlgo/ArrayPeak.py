019656e88320b52050a3fcdf0a4a7b0ffb1b702e
mohammadkeshavarzi <keshavarzi.m380@gmail.com>
2022-02-15 21:41:24 +0330

    Add `findFirstPeak` function

--- SpAlgo/ArrayPeak.py
+++ SpAlgo/ArrayPeak.py
@@ -54,10 +54,22 @@ class ArrayPeak:
             if (current_element > self._inner_array[i + 1]) and (current_element > self._inner_array[i - 1]):
                 list_of_peaks += [(self._inner_array[i], i)]
 
-        if self._inner_array[-1] > self._inner_array[-2]:
-            list_of_peaks += [(self._inner_array[-1], self._size - 1)]
+        if self._inner_array[self._size - 1] > self._inner_array[self._size - 2]:
+            list_of_peaks += [(self._inner_array[self._size-1], self._size - 1)]
 
         return list_of_peaks
+
+    def findFirstPeak(self):
+        if self._inner_array[0] > self._inner_array[1]:
+            return self._inner_array[0], 0
+        for i in range(1, self._size - 1):
+            current_element = self._inner_array[i]
+            if (current_element > self._inner_array[i + 1]) and (current_element > self._inner_array[i - 1]):
+                 return self._inner_array[i], i
+
+        if self._inner_array[self._size - 1] > self._inner_array[self._size - 2]:
+            return self._inner_array[self._size - 1], self._size - 1 
+        
     
 
 
