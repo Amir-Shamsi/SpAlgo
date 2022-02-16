fe77e3411e9b7dd4d77cb1a29261122ca7b585b0
mohammadkeshavarzi <keshavarzi.m380@gmail.com>
2022-02-16 16:05:17 +0330

    complete test for `ArrayPeak.py`

---
+++ tests/test_ArrayPeak_findPeak.py
@@ -0,0 +1,15 @@
+from SpAlgo import ArrayPeak
+
+def test_ArrayPeak():
+    array = [7, 2, 22, 3, 17, 5, 2, 9, 22, 4]
+    res = ArrayPeak.ArrayPeak(array)
+
+
+    # checking outputs
+
+    assert res.findAllPeaks() == [(7, 0), (22, 2), (17, 4), (22, 8)]
+    assert res.findFirstPeak() == (7, 0)
+    assert res.findMaxPeak() == (22, 2)
+    assert res.findMinPeak() == (7, 0)
+    assert res.findPeak() == (17, 4)
+    
