036d9399f1e28f58df087d039063109d6b1027da
mohammadkeshavarzi <keshavarzi.m380@gmail.com>
2022-02-17 12:09:30 +0330

    complete `test_MatrixPeak`

--- tests/test_MatrixPeak_findPeak.py
+++ tests/test_MatrixPeak_findPeak.py
@@ -1,13 +1,30 @@
 from SpAlgo.MatrixPeak import MatrixPeak
 
-def test_findLongestMaxSubSeq():
+
+def test_MatrixPeak_findPeak():
     """
     :return: None
     """
     _mat = [[10, 8, 10, 10],
-           [14, 13, 12, 11],
-           [15, 9, 11, 21],
-           [16, 17, 19, 20],
-           [5, 7, 11, 4]]
+            [14, 13, 12, 11],
+            [15, 9, 11, 21],
+            [16, 17, 19, 20],
+            [5, 7, 11, 4]]
     _matrix_peak = MatrixPeak(matrix=_mat)
     assert _matrix_peak.findPeak() == {'peak': 21, 'column': 3, 'row': 2}
+
+
+def test_MatrixPeak_findAllPeak():
+    """
+    :return: None
+    """
+    _mat = [[10, 18, 10, 10],
+            [14, 13, 12, 11],
+            [15, 9, 11, 21],
+            [20, 17, 19, 20],
+            [5, 7, 11, 4]]
+
+    _matrix_all_peak = MatrixPeak(matrix=_mat)
+    assert _matrix_all_peak.findAllPeak() == [{'peak': 18, 'column': 1, 'row': 0},
+                                              {'peak': 21, 'column': 3, 'row': 2},
+                                              {'peak': 20, 'column': 0, 'row': 3}]
