3b06df5a52485473e47683fd3b4604c2951eed75
Amir Shamsi <emirshamsi.uni@gmail.com>
2022-02-17 19:55:09 +0330

    Update Matrix_findAllPeak.py

---
+++ src/examples/Matrix_findAllPeak.py
@@ -0,0 +1,40 @@
+from SpAlgo import Matrix
+
+# find a peak in a given m * n matrix
+
+# color code to make the output look stoning
+ORANGE_COLOR = '\033[94m'
+
+# code of make the text bold
+BOLD_START = '\033[1m'
+
+# code of making text to go back to the normal
+NORM = '\033[0m'
+
+
+# example of a given matrix
+
+input_matrix = [[2, 4, 1, 7, 8],
+                [4, 7, 2, 1, 9],
+                [6, 12, 21, 3, 5],
+                [8, 10, 2, 20, 1]]
+
+'''
+    pass it into 'Matrix' class inside 'Matrix.py' and store
+    result in `matrix` variable
+'''
+
+matrixPeak = Matrix.Matrix(input_matrix)
+
+# get all peaks from matrixPeak
+allPeaks = matrixPeak.findAllPeak()
+
+# we print to see the result
+print('--------- Find All Peak Inside A Matrix ---------')
+
+
+# print results
+print("All peaks in matrix :", BOLD_START + ORANGE_COLOR, allPeaks, NORM)
+
+
+
