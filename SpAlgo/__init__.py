033fdf10f64e200a0fb84f6658df843a4cc64b78
Amir Shamsi <emirshamsi.uni@gmail.com>
2022-02-18 21:49:26 +0330

    `build-in` files name changed to lowercase

--- SpAlgo/__init__.py
+++ SpAlgo/__init__.py
@@ -1,11 +1,11 @@
 """
 The python source code.
 """
-from SpAlgo import _SubSeq
+from SpAlgo import _subseq
 
-from SpAlgo._Array import Array
-from SpAlgo._Matrix import Matrix
-from SpAlgo._SubSeq import LongestMax
+from SpAlgo._array import Array
+from SpAlgo._matrix import Matrix
+from SpAlgo._subseq import LongestMax
 
 __version__ = "0.1.1"
 __all__ = ['Matrix', 'LongestMax', 'Array']
