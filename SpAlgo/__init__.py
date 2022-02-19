a4c2f739d65116bff29750b5532007ea0240f762
Amir Shamsi <emirshamsi.uni@gmail.com>
2022-02-19 09:35:46 +0330

    Update __init__.py

--- SpAlgo/__init__.py
+++ SpAlgo/__init__.py
@@ -1,7 +1,6 @@
 """
 The python source code.
 """
-from SpAlgo import _subseq
 
 from SpAlgo._array import Array
 from SpAlgo._matrix import Matrix
@@ -9,4 +8,3 @@ from SpAlgo._subseq import LongestMax
 
 __version__ = "0.1.1"
 __all__ = ['Matrix', 'LongestMax', 'Array']
-
