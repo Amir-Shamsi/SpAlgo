7bce1f7fa440140b1f2474a55188a1a0cfed37e9
Amir Shamsi <emirshamsi.uni@gmail.com>
2022-04-02 12:33:43 +0430

    update __init__ to handle `ClosestPair` & `Point`

--- SpAlgo/__init__.py
+++ SpAlgo/__init__.py
@@ -6,5 +6,9 @@ from SpAlgo._peak._array import Array
 from SpAlgo._peak._matrix import Matrix
 from SpAlgo._sequences._sequence import MaxSubseq
 from SpAlgo._knapsack._knapsack import Knapsack
+from SpAlgo._closest_pair._closest import ClosestPair
+from SpAlgo._closest_pair._point import Point
 
-__version__ = "0.2.1"
+__version__ = "0.3.0"
+__all__ = ['Matrix', 'MaxSubseq', 'Array', 'Knapsack', 'ClosestPair',
+           'Point']
