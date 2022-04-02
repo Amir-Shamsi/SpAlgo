14fe15dc5a6ccacdce5ea964b4a11daf5b462eca
Amir Shamsi <emirshamsi.uni@gmail.com>
2022-04-02 13:47:35 +0430

    example for `get_closest_pair` added

--- src/examples/ClosestPair.py
+++ src/examples/ClosestPair.py
@@ -28,8 +28,22 @@ closest_pair = ClosestPair(points_set)
 # getting minimum distance
 min_distance = closest_pair.get_min_distance()
 
+"""
+getting pairs those have the minimum distance
+    OPTIONAL =>
+        :argument _type: its default is Point and it will return list of Points otherwise
+                         enter types list or tuple
+                         
+            _type = list  -> e.g. [[7, 0], [11, 0]]
+            _type = tuple -> e.g. [(7, 0), (11, 0)]
+            _type = Point -> e.g. [Point(7, 0), Point(11, 0)]
+"""
+pairs = closest_pair.get_closest_pair(_type=Point)
+
 # we print to see the result
 print('--------- ClosestPair Algorithm ---------')
-
 # printing the result
-print('The minimum distance of the points set is', BOLD_START + ORANGE_COLOR, min_distance, NORM)
+print('The minimum distance of the points set is', BOLD_START + ORANGE_COLOR, min_distance, NORM,
+      '\nand found the pairs are:\n' + BOLD_START + 'First Point: ' + ORANGE_COLOR +
+      '{}, {}'.format(pairs[0].x, pairs[0].y) + NORM +
+      BOLD_START + '\nSecond Point: ' + ORANGE_COLOR +'{}, {}'.format(pairs[1].x, pairs[1].y) + NORM)
