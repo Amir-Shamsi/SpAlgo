d60b7837bf26829a06343485b2b7d7c1321c8d28
Amir Shamsi <emirshamsi.uni@gmail.com>
2022-04-02 12:25:31 +0430

    update package `ref` inside main __init__

--- SpAlgo/__init__.py
+++ SpAlgo/__init__.py
@@ -2,10 +2,9 @@
 The python source code.
 """
 
-from SpAlgo._array import Array
-from SpAlgo._matrix import Matrix
-from SpAlgo._sequence import MaxSubseq
+from SpAlgo._peak._array import Array
+from SpAlgo._peak._matrix import Matrix
+from SpAlgo._sequences._sequence import MaxSubseq
 from SpAlgo._knapsack._knapsack import Knapsack
 
 __version__ = "0.2.1"
-__all__ = ['Matrix', 'MaxSubseq', 'Array', 'Knapsack']
